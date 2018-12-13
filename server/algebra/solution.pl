2ddd4b199d934f27f1a86b79cfeb0207c71da04d
Charles Shuller <cshuller@connectionsacademy.com>
2018-12-13 17:12:19 -0600

    Altered to remove detection of too many repeated restatements, and created basic default rule stating it doesn't recognize the transform or computation

--- server/algebra/solution.pl
+++ server/algebra/solution.pl
@@ -1,6 +1,8 @@
 %% -*-prolog-*-
 %%
 :- module(solution, [solution/5, solution/7]).
+:- use_module(algebra_rules).
+
 
 %% Helper function for solution 7, this is what should be called from external code.
 solution(Problem, MinNumSteps, MaxNumSteps, PossibleSolutions, ResultsOut) :-
@@ -20,8 +22,8 @@ solution(Problem, MinNumSteps, MaxNumSteps, PossibleSolutions, ResultsOut) :-
 %%
 %% CurrentStep, the number of steps we've been through.
 %%
-%% PreviousPossibleSolutionLine is the previous line of the possible solution, for the first call, it should be
-%% the sam as Problem, and has the same form as an inner list for PossibleSolutions.
+%% PreviousStudentWorkLine is the previous line of the possible solution, for the first call, it should be
+%% the sam as Problem, and has the same form as an inner list for StudentWork.
 %%
 %% PossibleSolution is a list of lists of tokens for algebraic primitives (1, 2, 3, x, y, z, +, -, =, etc....).
 %% It is of the form:
@@ -37,38 +39,65 @@ solution(Problem, MinNumSteps, MaxNumSteps, PossibleSolutions, ResultsOut) :-
 %%
 %%
 %%
-solution(_Problem, _MinNumSteps, MaxNumSteps, _PreviousPossibleSolutionLine, _PossibleSolutions, ResultsIn, ResultsOut) :-
+
+%%
+%% Identify total steps above maximum number of steps
+%%
+solution(_Problem, _MinNumSteps, MaxNumSteps, _PreviousStudentWorkLine, _StudentWork, ResultsIn, ResultsOut) :-
     results{step_count:StepCount, error_count:ErrorCount, messages:Messages} = ResultsIn,
     StepCount > MaxNumSteps,
-    NewMessages = [ ["Max Steps Exceeded"] | Messages ],
+    append(Messages, ["Too Many Steps"], NewMessages),
     NewErrorCount is ErrorCount + 1,
     NewStepCount is StepCount + 1,
     ResultsOut = results{step_count:NewStepCount, error_count:NewErrorCount, messages:NewMessages}.
 
 
-solution(_Problem, MinNumSteps, _MaxNumSteps, _PreviousPossibleSolutionLine, PossibleSolutions, ResultsIn, ResultsOut) :-
-    PossibleSolutions = [],
+%%
+%% Identify total steps below minimum number of steps
+%%
+solution(_Problem, MinNumSteps, _MaxNumSteps, _PreviousStudentWorkLine, StudentWork, ResultsIn, ResultsOut) :-
+    StudentWork = [],
     results{step_count:StepCount, error_count:ErrorCount, messages:Messages} = ResultsIn,
     StepCount < MinNumSteps,
-    NewMessages = [ ["Not Enough Steps"] | Messages ],
+    append(Messages, ["Too Few Steps"], NewMessages),
     NewErrorCount is ErrorCount + 1,
     NewStepCount is StepCount + 1,
     ResultsOut = results{step_count:NewStepCount, error_count:NewErrorCount, messages:NewMessages}.
 
-solution(Problem, _MinNumSteps, _MaxNumSteps, Problem, [Problem | _], ResultsIn, ResultsOut) :-
+%%
+%% Identify end of student work, Check to see if it's simplified, and set results out to results in
+%%
+solution(_Problem, _MinNumSteps, _MaxNumSteps, _PreviousStudentWorkLine, [], ResultsIn, ResultsOut) :-
+    %Should check for simplified solution
+    ResultsOut = ResultsIn.
+
+
+%%
+%% Identify when the student has just re-stated the problem.
+%%
+solution(Problem, MinNumSteps, MaxNumSteps, _PreviousStudentWorkLine, [StudentWorkLine | RemainingStudentWork], ResultsIn, ResultsOut) :-
     results{step_count:StepCount, error_count:ErrorCount, messages:Messages} = ResultsIn,
-    NewMessages = [ ["Problem Restated Too Many Times"] | Messages ],
-    NewErrorCount is ErrorCount + 1,
+
+    algebra_rules:problem_restatement(Problem, StudentWorkLine),
+
+    append(Messages, ["Problem Restatement"], NewMessages),
+    NewErrorCount = ErrorCount,
     NewStepCount is StepCount + 1,
-    ResultsOut = results{step_count:NewStepCount, error_count:NewErrorCount, messages:NewMessages}.
+    Results = results{step_count:NewStepCount, error_count:NewErrorCount, messages:NewMessages},
+    solution(Problem, MinNumSteps, MaxNumSteps, StudentWorkLine, RemainingStudentWork, Results, ResultsOut).
 
-
-solution(_Problem,
-	 _MinNumSteps,
-	 _MaxNumSteps,
-	 _PreviousPossibleSolutionLine,
-	 _PossibleSolutions,
-	 _ResultsIn,
+solution(Problem,
+	 MinNumSteps,
+	 MaxNumSteps,
+	 _PreviousStudentWorkLine,
+	 [StudentWorkLine | RemainingStudentWork],
+	 ResultsIn,
 	 ResultsOut) :-
 
-    ResultsOut = true.
+    results{step_count:StepCount, error_count:ErrorCount, messages:Messages} = ResultsIn,
+
+    append(Messages, ["Unrecognized Transform or Computation"], NewMessages),
+    NewErrorCount = ErrorCount,
+    NewStepCount is StepCount + 1,
+    Results = results{step_count:NewStepCount, error_count:NewErrorCount, messages:NewMessages},
+    solution(Problem, MinNumSteps, MaxNumSteps, StudentWorkLine, RemainingStudentWork, Results, ResultsOut).
